8e23b6c906f83fa142fdb6f7df0c40c2d3909b4e
Sirmetana <lukas.siopathis@gmail.com>
2023-09-12 11:01:09 +0200

    tentative timer

--- pomodoro.js
+++ pomodoro.js
@@ -1,16 +1,39 @@
 let timeLimitInMinutes = 10;
 let timeLimitInSeconds = timeLimitInMinutes * 60;
 let timerElement = document.getElementById('timer');
+let compteurCycles = 0;
+let enCours = 0;
+
+function setReset()
+{
+  console.log("Entrée Fonction setRest. enCours = " + enCours);
+  if(enCours ==1)
+  {
+    duree = document.getElementById("timerDuree").textContent;
+    //duree = duree.substring(0,1) + ':' + duree.substring(2,3);
+    document.getElementById("timerButton").value = "Lancer";
+    timeLimitInMinutes = duree.substring(0,1);
+    timeLimitInSeconds = duree.substring(2,3);
+    console.log("Timer reset");
+  }
+  else
+  {
+    document.getElementById("timerButton").value = "Relancer";
+    console.log("Timer lancé");
+    lancer();
+  }
+}
 
 function lancer()
 {
-    timeLimitInSeconds--;
-    let minutes = Math.floor(timeLimitInSeconds / 60);
-    let seconds = timeLimitInSeconds % 60;
+  timeLimitInSeconds--;
+  let minutes = Math.floor(timeLimitInSeconds / 60);
+  let seconds = timeLimitInSeconds % 60;
 
   if (timeLimitInSeconds < 0) {
     timerElement.textContent = '00:00';
     clearInterval(timerInterval);
+    enCours = 0;
     return;
   }
 
@@ -24,4 +47,4 @@ function lancer()
   timerElement.textContent = minutes + ':' + seconds;
 }
 
-var timerInterval = setInterval(startTimer, 1000);
+let timerInterval = setInterval(lancer, 1000);
