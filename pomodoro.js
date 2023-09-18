73504b56dbe7b2bfdfaa5e62e10e3fb220551f36
Sirmetana <lukas.siopathis@gmail.com>
2023-09-18 10:00:35 +0200

    Début css

--- pomodoro.js
+++ pomodoro.js
@@ -1,15 +1,18 @@
 let dureeTrav = document.getElementById('timerDureeTrav').value;
 let dureePause = document.getElementById('timerDureePause').value;
+
 let timeLimitInMinutes = dureeTrav;
 let timeLimitInSeconds = timeLimitInMinutes * 60;
 let timerElement = document.getElementById('timer');
-if(parseInt(dureeTrav) <10)
+
+if(parseInt(dureeTrav) <10) //Formate le chrono en 'mm:ss'
 {
   dureeTrav = '0' + dureeTrav;
   if(dureeTrav==0)
     dureeTrav = '0' + dureeTrav;
 }
 timerElement.textContent = dureeTrav + ':00';
+
 let enCours = 0;  //Si timer tourne ou non
 let activite = 1; //Pause : 0, Travail : 1
 let premierLancer = 0; //Empêche le timer de se décompter plusieurs fois
@@ -17,12 +20,13 @@ let premierLancer = 0; //Empêche le timer de se décompter plusieurs fois
 function startTimer() {
   //console.log("startTimer() " + enCours);
 
-  if(enCours ==1)
+  if(enCours ==1)   //Fait tourner le chrono lorsqu'il n'y a pas de pause
   {
     timeLimitInSeconds--;
 
-    if (timeLimitInSeconds < 0) {
-      console.log("timer à 0. Activité : " + activite);
+    if (timeLimitInSeconds < 0) { //A la fin du chrono, passe d'une activité à l'autre
+      //console.log("timer à 0. Activité : " + activite);
+
       if(activite ==1)
       {
         //console.log("Travail => Pause : "+ dureePause);
@@ -42,8 +46,9 @@ function startTimer() {
         document.getElementById('activite').textContent = "Travail";
         }
       let enCours = 0;
-      //return;
     }
+
+    //Formatage
     let minutes = Math.floor(timeLimitInSeconds / 60);
     let seconds = timeLimitInSeconds % 60;
 
@@ -59,11 +64,11 @@ function startTimer() {
   
 }
 
-function lancer()
+function lancer() //Lance le chrono et change le bouton Lancer/Pause
 {
-  if(enCours == 0)
+  if(enCours == 0)  //Vérifie si le chrono tourne
   {  
-    if(premierLancer==0)
+    if(premierLancer==0)  //Lance le chrono pour la première fois
     {
       let timerInterval = setInterval(startTimer, 1000);
       premierLancer =1;
@@ -80,7 +85,7 @@ function lancer()
 
 }
 
-function setReset()
+function setReset() //Réinitialise le chrono à la durée de travail
 {
   //console.log("setReset() " + enCours);
   timeLimitInMinutes = document.getElementById('timerDureeTrav').value;
